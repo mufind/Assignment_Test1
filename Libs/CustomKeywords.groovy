7576aadaabb2811240af57173e6b5ab541fc373a
mufind <107871444+mufind@users.noreply.github.com>
2022-08-01 10:01:00 +0700

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,65 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+
+ /**
+	 * Create Header basic authentication property,
+	 * this property value is Base64 encoded token from user name and password 
+	 * @param usernameAdmin user name
+	 * @param password password password
+	 * @return Katalon object property, an instance of TestObjectProperty 
+	 */ 
+def static "com.example.WebApiCustomKeywords.createBasicAuthProperty"(
+    	String usernameAdmin	
+     , 	String password	) {
+    (new com.example.WebApiCustomKeywords()).createBasicAuthProperty(
+        	usernameAdmin
+         , 	password)
+}
+
+ /**
+	 * Create HTTP body content in json format to update existing user
+	 * @param password Account password
+	 * @param emailAddress
+	 * @param displayName A friendly user name
+	 * @param notification Any comment
+	 * @return Json string generated from inputed data
+	 */ 
+def static "com.example.WebApiCustomKeywords.updateHttpBody"(
+    	String password	
+     , 	String emailAddress	
+     , 	String displayName	
+     , 	String notification	) {
+    (new com.example.WebApiCustomKeywords()).updateHttpBody(
+        	password
+         , 	emailAddress
+         , 	displayName
+         , 	notification)
+}
+
+ /**
+	 * Create HTTP body content in json format to create new user 
+	 * @param username Account name
+	 * @param password Account password
+	 * @param emailAddress 
+	 * @param displayName A friendly user name
+	 * @param notification Any comment
+	 * @return Json string generated from inputed data
+	 */ 
+def static "com.example.WebApiCustomKeywords.newHttpBody"(
+    	String username	
+     , 	String password	
+     , 	String emailAddress	
+     , 	String displayName	
+     , 	String notification	) {
+    (new com.example.WebApiCustomKeywords()).newHttpBody(
+        	username
+         , 	password
+         , 	emailAddress
+         , 	displayName
+         , 	notification)
+}
---
+++ Test Suites/TS_reqres.in.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_reqres.in</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f28b5bf5-5b3c-4bc0-9f61-26875c5d16a0</testSuiteGuid>
+   <testCaseLink>
+      <guid>b1fc229c-a86e-4699-8c36-4975917b21ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_GET_Verify List Users</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6815c94-1536-4930-9bab-ca5b431c5929</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02_GET_Verify Single User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>709453c8-a05d-42fe-82d2-100038a936f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03_PUT_Verify Update</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49dcc75e-e52f-4296-a24d-555e348c8c72</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04_POST_Verify Register Successful</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
